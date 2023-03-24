69e20ea7276ff3ce6b610ad5836ae8338d8d4386
Curtis Mason <curtis@chula.ai>
2023-03-24 16:26:10 -0400

    renamed pipebird

--- deploy/scripts/update_s3/__main__.py
+++ deploy/scripts/update_s3/__main__.py
@@ -12,7 +12,7 @@ load_dotenv()  # take environment variables from .env.
 FOLDER_PATHS = ['./deploy/aws/cloudformation']
 S3_USER_ACCESS_ID = os.getenv('S3_USER_ACCESS_ID')
 S3_USER_SECRET_KEY = os.getenv('S3_USER_SECRET_KEY')
-PIPEBIRD_CONFIG_BUCKET_NAME = os.getenv('PIPEBIRD_CONFIG_BUCKET_NAME')
+OLLI_CONFIG_BUCKET_NAME = os.getenv('OLLI_CONFIG_BUCKET_NAME')
 
 
 class Version(TypedDict):
@@ -25,7 +25,7 @@ if __name__ == '__main__':
         assert isinstance(FOLDER_PATH, str) and \
             isinstance(S3_USER_ACCESS_ID, str) and \
             isinstance(S3_USER_SECRET_KEY, str) and \
-            isinstance(PIPEBIRD_CONFIG_BUCKET_NAME, str)
+            isinstance(OLLI_CONFIG_BUCKET_NAME, str)
         filepaths: List[str] = [os.path.join(FOLDER_PATH, file) for file in os.listdir(FOLDER_PATH)]
         object_names = [file.split('.')[0] for file in os.listdir(FOLDER_PATH)]
         #Creating Session With Boto3.
@@ -36,4 +36,4 @@ if __name__ == '__main__':
         # Creating S3 Resource From the Session.
         s3 = session.resource('s3') 
         for i, file in enumerate(filepaths):
-            s3.Bucket(PIPEBIRD_CONFIG_BUCKET_NAME).upload_file(file, object_names[i], ExtraArgs={'ACL':'public-read'})
+            s3.Bucket(OLLI_CONFIG_BUCKET_NAME).upload_file(file, object_names[i], ExtraArgs={'ACL':'public-read'})
